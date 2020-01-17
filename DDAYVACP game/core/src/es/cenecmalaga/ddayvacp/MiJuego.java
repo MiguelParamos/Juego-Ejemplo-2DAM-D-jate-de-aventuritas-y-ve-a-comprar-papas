1679939ea196b2093faad2b04e3cc3a1c2e380b8
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-01-17 11:04:53 +0100

    Primer Sprite
    Puesto un primer Sprite siempre en el centro, que se ajusta al zoom que tengamos en el mapa.

--- DDAYVACP game/core/src/es/cenecmalaga/ddayvacp/MiJuego.java
+++ DDAYVACP game/core/src/es/cenecmalaga/ddayvacp/MiJuego.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 import es.cenecmalaga.entrada.EscuchadorTeclado;
 
@@ -25,11 +27,18 @@ public class MiJuego extends ApplicationAdapter {
 	private static int WIDTH; //Aquí almacenaremos la anchura en tiles
 	private static int HEIGHT; //Aquí almacenaremos la altura en tiles
 	public static final float unitScale = 1/16f; //Nos servirá para establecer que la pantalla se divide en tiles de 16 pixeles;
+	private Sprite jugador; //Sprite del jugador sobre el mapa
+	private int pixelsMapaAncho; //Calculo en ella el ancho del mapa en pixels
+	private int pixelsMapaAlto; //Calculo en ella el alto del mapa en pixels
+	private Texture texturaJugador; //La textura del jugador.
+
 	/**
 	 * Sirve para inicializar variables, como el mundo, personajes, teclado, sprites...
 	 */
 	@Override
 	public void create () {
+
+		//CREACIóN DEL MUNDO
 		batch = new SpriteBatch();
 		float w = Gdx.graphics.getWidth(); //Obtenemos la anchura de nuestra pantalla en pixels
 		float h = Gdx.graphics.getHeight(); //Obtenemos la atura de nuestra pantalla en pixels
@@ -39,6 +48,8 @@ public class MiJuego extends ApplicationAdapter {
 		camera.zoom=0.3f; //Establecemos el zoom de la cámara. 0.1 es más cercano que 1.
 		WIDTH = ((TiledMapTileLayer) map.getLayers().get(0)).getWidth(); //Obtenemos desde el mapa el número de tiles de ancho de la 1º Capa
 		HEIGHT = ((TiledMapTileLayer) map.getLayers().get(0)).getHeight(); //Obtenemos desde el mapa el número de tiles de alto de la 1º Capa
+		pixelsMapaAncho=WIDTH*(int)map.getProperties().get("width");
+		pixelsMapaAlto=HEIGHT*(int)map.getProperties().get("height");
 
 		Gdx.app.log("Width",Float.toString(WIDTH)); //Sacamos por el log el número de tiles de ancho
 		Gdx.app.log("Height",Float.toString(HEIGHT)); //Sacamos por el log el número de tiles de alto
@@ -50,9 +61,29 @@ public class MiJuego extends ApplicationAdapter {
 		Gdx.app.log("camera x",Float.toString(camera.position.y));
 		camera.update(); //Colocamos la Cámara.
 
+		//CREACIÓN DE PERSONAJES
+		texturaJugador=
+				new Texture("personajes/playerMale.png");
+		jugador=new Sprite(texturaJugador);
+		Vector3 posicionJugadorTiles=new Vector3(
+				camera.position.x,camera.position.y,0);
+		Vector3 posicionJugadorPixels=camera.project(posicionJugadorTiles,0,0,w,h);
+		jugador.setPosition(posicionJugadorPixels.x,posicionJugadorPixels.y);
+
+
+
+		jugador.setSize(
+			((Gdx.graphics.getWidth()*texturaJugador.getWidth())
+					/pixelsMapaAncho)*(1/camera.zoom),
+				((Gdx.graphics.getHeight()*texturaJugador.getHeight())
+						/pixelsMapaAlto)
+				*(1/camera.zoom));
+
+		//ESCUCHADORES
 		InputMultiplexer multiplexer = new InputMultiplexer();
 		multiplexer.addProcessor(new EscuchadorTeclado(camera,map));
 		Gdx.input.setInputProcessor(multiplexer);
+
 	}
 
 	/**Función que se ejecuta 60 veces por segundo por defecto, y dibuja
@@ -60,18 +91,32 @@ public class MiJuego extends ApplicationAdapter {
 	 */
 	@Override
 	public void render () {
+
 		//Color de limpieza, evita que cuando se desplaza la cámara
 		//se queden dibujos antiguos que ya no deberían estar alli.
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		//limpia el fondo de pantalla con el color indicado
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//indica los elementos que se meten en el batch
+
+		camera.update();
+		renderer.render(); //Renderizamos la vista
+
+		//Recalculo tamaño del sprite de jugador
+		jugador.setSize(
+		((Gdx.graphics.getWidth()*texturaJugador.getWidth())
+				/pixelsMapaAncho)*(1/camera.zoom),
+		((Gdx.graphics.getHeight()*texturaJugador.getHeight())
+				/pixelsMapaAlto)
+				*(1/camera.zoom));
+
+
 		batch.begin();
+		renderer.setView(camera); //Establecemos la vista del mundo a través de la cámara.
+
+		jugador.draw(batch);
 		//End termina y dibuja.
 		batch.end();
-		camera.update();
-		renderer.setView(camera); //Establecemos la vista del mundo a través de la cámara.
-		renderer.render(); //Renderizamos la vista
 	}
 
 	/**
--- DDAYVACP game/core/src/es/cenecmalaga/entrada/EscuchadorTeclado.java
+++ DDAYVACP game/core/src/es/cenecmalaga/entrada/EscuchadorTeclado.java
@@ -1,9 +1,11 @@
 package es.cenecmalaga.entrada;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector3;
 
 public class EscuchadorTeclado implements InputProcessor {
     private OrthographicCamera camera; //Aquí capturaremos la cámara de la clase del juego
@@ -48,6 +50,12 @@ public class EscuchadorTeclado implements InputProcessor {
                 //en cuadrados en todo el borde del mapa.
 
                 break;
+            case Keys.PLUS:
+                camera.zoom = Math.max(camera.zoom - 0.2f, 0.1f); //Se hace zoom entre 0.1 y 2
+                break;
+            case Keys.MINUS:
+                camera.zoom = Math.min(camera.zoom + 0.2f, 2); //Se hace zoom entre 0.1 y 2
+                break;
         }
         camera.update();
         return true;
@@ -60,6 +68,13 @@ public class EscuchadorTeclado implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Gdx.app.log("touchdown",screenX+" , "+screenX+" | "+pointer+" | "+button);
+        Vector3 unprojection=camera.unproject(new Vector3(screenX,screenY,0));  //Convertimos los píxeles de la pulsación a tiles
+        unprojection.x=(float)Math.floor(unprojection.x); //Hacemos Floor para que nos dé el tile exacto, sin decimales.
+        unprojection.y=(float)Math.floor(unprojection.y); //Hacemos Floor para que nos dé el tile exacto, sin decimales.
+        camera.position.x=unprojection.x; //Establecemos la cámara a la posic.
+        camera.position.y=unprojection.y; //Establecemos la cámara a la posic.
+        Gdx.app.log("unprojection",unprojection.x+" , "+unprojection.y+" , "+unprojection.z);
         return false;
     }
 
